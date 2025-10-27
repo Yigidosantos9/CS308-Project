19ea830fbfb6b612d4fb8dcaa43fdba68fdc988c
yigitdemircioglu <yigit.demirciogluu@gmail.com>
2025-10-27 19:05:59 +0300

    Initial commit
    Api is created.

---
+++ auto-next-authentication-api/src/main/java/com/codingapp/autonextauthenticationapi/AutoNextAuthenticationApiApplication.java
@@ -0,0 +1,13 @@
+package com.codingapp.autonextauthenticationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoNextAuthenticationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoNextAuthenticationApiApplication.class, args);
+    }
+
+}
---
+++ auto-next-authentication-api/src/test/java/com/codingapp/autonextauthenticationapi/AutoNextAuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingapp.autonextauthenticationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoNextAuthenticationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
